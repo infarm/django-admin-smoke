bc3c75275079b767e96861542e761f385edeb668
Nicolas Delaby <nicolas.delaby@infarm.com>
2024-01-22 17:41:59 +0100

    Test and approve support for django5.0

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import subprocess
 from setuptools import setup  # type: ignore
 from pathlib import Path
 
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
-version_re = re.compile('^Version: (.+)$', re.M)
-package_name = 'django-admin-smoke'
+version_re = re.compile("^Version: (.+)$", re.M)
+package_name = "django-admin-smoke"
 
 
 def get_version():
@@ -18,38 +18,37 @@ def get_version():
     https://gist.github.com/pwithnall/7bc5f320b3bdf418265a
     """
     d: Path = Path(__file__).absolute().parent
-    git_dir = d.joinpath('.git')
+    git_dir = d.joinpath(".git")
     if git_dir.is_dir():
         # Get the version using "git describe".
-        cmd = 'git describe --tags --match [0-9]*'.split()
+        cmd = "git describe --tags --match [0-9]*".split()
         try:
             version = subprocess.check_output(cmd).decode().strip()
         except subprocess.CalledProcessError:
             return None
 
         # PEP 386 compatibility
-        if '-' in version:
-            version = '.post'.join(version.split('-')[:2])
+        if "-" in version:
+            version = ".post".join(version.split("-")[:2])
 
         # Don't declare a version "dirty" merely because a time stamp has
         # changed. If it is dirty, append a ".dev1" suffix to indicate a
         # development revision after the release.
-        with open(os.devnull, 'w') as fd_devnull:
-            subprocess.call(['git', 'status'],
-                            stdout=fd_devnull, stderr=fd_devnull)
+        with open(os.devnull, "w") as fd_devnull:
+            subprocess.call(["git", "status"], stdout=fd_devnull, stderr=fd_devnull)
 
-        cmd = 'git diff-index --name-only HEAD'.split()
+        cmd = "git diff-index --name-only HEAD".split()
         try:
             dirty = subprocess.check_output(cmd).decode().strip()
         except subprocess.CalledProcessError:
             return None
 
-        if dirty != '':
-            version += '.dev1'
+        if dirty != "":
+            version += ".dev1"
     else:
         # Extract the version from the PKG-INFO file.
         try:
-            with open('PKG-INFO') as v:
+            with open("PKG-INFO") as v:
                 version = version_re.search(v.read()).group(1)
         except FileNotFoundError:
             version = None
@@ -69,20 +68,22 @@ setup(
     author_email="zimbler@gmail.com",
     description="Smoke tests for Django admin",
     install_requires=[
-        "Django>=4.2,<5.0",
+        "Django>=4.2,<5.1",
         "django-testing-utils",
     ],
     dependency_links=[
-        "git+https://github.com/infarm/django-testing-utils.git@django4.2#egg=django-testing-utils",
+        "git+https://github.com/infarm/django-testing-utils.git@django5.0#egg=django-testing-utils",
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Testing",
     ],
 )
